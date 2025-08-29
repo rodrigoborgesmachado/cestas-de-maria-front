9457b2a86f2ab5fd1ed8ca7c18441d0dd1f81b23
Rodrigo Machado <rodrigoborgesmachado@gmail.com>
2025-08-29 14:31:04 -0300

    Colocando opção de confirmar filtro com enter.

--- src/components/admin/FilterComponent/FilterComponent.js
+++ src/components/admin/FilterComponent/FilterComponent.js
@@ -24,7 +24,9 @@ const FilterComponent = ({ placeHolder, showTermFilter, showStartDate=false, sho
     }));
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
     submitFilter({
       term: term || undefined,
       startDate: startDate || undefined,
@@ -67,104 +69,106 @@ const FilterComponent = ({ placeHolder, showTermFilter, showStartDate=false, sho
 
   return (
     <div className="filter-container">
-      <div className='filter-container-main'>
-        <div className='filter-itens'>
-          <div className='filter-basic'>
-            {showTermFilter && (
-              <div className='filter-basic-item'>
-                <span>Filtro</span>
-                <input
-                  type="text"
-                  placeholder={placeHolder}
-                  className="main-input"
-                  value={term}
-                  onChange={(e) => setTerm(e.target.value)}
-                />
-              </div>
-            )}
-            {showStartDate && (
-              <div className='filter-basic-item'>
-                <span>Data Inicial</span>
-                <input
-                  type="date"
-                  placeholder="Data Inicial"
-                  className="main-input"
-                  value={startDate}
-                  onChange={(e) => setStartDate(e.target.value)}
-                />
-              </div>
-            )}
-            {showEndDate && (
-              <div className='filter-basic-item'>
-                <span>Data Final</span>
-                <input
-                  type="date"
-                  placeholder="Data Final"
-                  className="main-input"
-                  value={endDate}
-                  onChange={(e) => setEndDate(e.target.value)}
-                />
-              </div>
-            )}
+      <form onSubmit={handleSubmit}>
+        <div className='filter-container-main'>
+          <div className='filter-itens'>
+            <div className='filter-basic'>
+              {showTermFilter && (
+                <div className='filter-basic-item'>
+                  <span>Filtro</span>
+                  <input
+                    type="text"
+                    placeholder={placeHolder}
+                    className="main-input"
+                    value={term}
+                    onChange={(e) => setTerm(e.target.value)}
+                  />
+                </div>
+              )}
+              {showStartDate && (
+                <div className='filter-basic-item'>
+                  <span>Data Inicial</span>
+                  <input
+                    type="date"
+                    placeholder="Data Inicial"
+                    className="main-input"
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                  />
+                </div>
+              )}
+              {showEndDate && (
+                <div className='filter-basic-item'>
+                  <span>Data Final</span>
+                  <input
+                    type="date"
+                    placeholder="Data Final"
+                    className="main-input"
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className='filter-buttons'>
+            <button type='submit' className="main-button margin-top-default">
+              Filtrar
+            </button>
+            {
+              exportFunction && (
+                <button onClick={exportReport} className="main-button margin-top-default">
+                  Exportar
+                </button>
+              )
+            }
+
+            {
+              showFamilyStatus ? 
+                <button onClick={() => setShowMoreFilter(prev => !prev)} className="main-button margin-top-default">
+                  {
+                    showMoreFilter ? 
+                    'Menos Filtros'
+                    :
+                    'Mais Filtros'
+                  }
+                </button>
+                : 
+                <></>
+            }
           </div>
         </div>
 
-        <div className='filter-buttons'>
-          <button onClick={handleSubmit} className="main-button margin-top-default">
-            Filtrar
-          </button>
-          {
-            exportFunction && (
-              <button onClick={exportReport} className="main-button margin-top-default">
-                Exportar
-              </button>
-            )
-          }
+        {
+          showMoreFilter &&
+          <div className='filters-more'>
+            {
+              showFamilyStatus && 
+              <div className="filters-more-item">
+                  <label>
+                      Status:
+                  </label>
 
-          {
-            showFamilyStatus ? 
-              <button onClick={() => setShowMoreFilter(prev => !prev)} className="main-button margin-top-default">
-                {
-                  showMoreFilter ? 
-                  'Menos Filtros'
-                  :
-                  'Mais Filtros'
-                }
-              </button>
-              : 
-              <></>
-          }
-        </div>
-      </div>
-
-      {
-        showMoreFilter &&
-        <div className='filters-more'>
-          {
-            showFamilyStatus && 
-            <div className="filters-more-item">
-                <label>
-                    Status:
-                </label>
-
-                <select
-                    name="familyStatusId"
-                    className="main-input"
-                    value={filter.ModelCode}
-                    onChange={handleInputChange}
-                    required
-                >
-                    <option value="">Selecione o Status</option>
-                    {familyStatusList.map((obj) => (
-                        <option key={obj.Id} value={obj.Id}>
-                            {obj.Description}
-                        </option>
-                    ))}
-                </select>
-            </div>
-          }
-        </div>
-      }
+                  <select
+                      name="familyStatusId"
+                      className="main-input"
+                      value={filter.ModelCode}
+                      onChange={handleInputChange}
+                      required
+                  >
+                      <option value="">Selecione o Status</option>
+                      {familyStatusList.map((obj) => (
+                          <option key={obj.Id} value={obj.Id}>
+                              {obj.Description}
+                          </option>
+                      ))}
+                  </select>
+              </div>
+            }
+          </div>
+        }
+      </form>
     </div>
   );
 };
